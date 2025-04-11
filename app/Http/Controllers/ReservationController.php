57031db20d42dcf965d2903337f7477024d19275
BleuSkell <kyasowi1@gmail.com>
2025-04-11 12:34:39 +0200

    sp works with the index method

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
     public function index()
-    {
-        return view('reservations.index');
+    {   
+        $userId = auth()->user()->id; // haal het id van de ingelogde user op
+
+        $reservations = DB::select('CALL sp_get_all_reservations_by_user_id(?)', [$userId]); // stuur de user id mee aan de sp
+
+        return view('reservations.index', ['reservations' => $reservations]); // return de view met de reservations
     }
 
     public function edit()
