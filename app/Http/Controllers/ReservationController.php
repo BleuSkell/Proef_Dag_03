6cb4e37e61b0f9f136621537d1ec882f44c6aff3
BleuSkell <kyasowi1@gmail.com>
2025-04-11 12:25:42 +0200

    added routes for index, edit, and update

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('reservations.index');
+    }
+
+    public function edit()
+    {
+        return view('reservations.edit');
+    }
+
+    public function update()
+    {
+        //
+    }
 }
---
+++ routes/reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth')->group(function () {
+    Route::get('/reserveringen', [ReservationController::class, 'index'])->name('reservations.index');
+    Route::get('/reserveringen/edit/{id}', [ReservationController::class, 'edit'])->name('reservations.edit');
+    Route::patch('/reserveringen/update/{id}', [ReservationController::class, 'update'])->name('reservations.update');
+});
