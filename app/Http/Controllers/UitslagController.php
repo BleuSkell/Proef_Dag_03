9eb47cc0e8249bd6ed41a8f7c4ec835274501783
Artycurus <339710@student.mboutrecht.nl>
2025-04-11 16:02:20 +0200

    Refactor UitslagController index method for improved date filtering and response handling; update DatabaseSeeder to check for existing user before creation

--- app/Http/Controllers/UitslagController.php
+++ app/Http/Controllers/UitslagController.php
@@ -9,8 +9,23 @@ class UitslagController extends Controller
 {
     public function index(Request $request)
     {
-        
+        $selectedDate = $request->input('date');
 
-        return view('uitslagen.index');
+        if ($selectedDate) {
+            $uitslagen = Uitslag::with(['reservering', 'persoon', 'spel'])
+                ->whereHas('reservering', function ($query) use ($selectedDate) {
+                    $query->whereDate('created_at', $selectedDate);
+                })
+                ->orderByDesc('aantalpunten')
+                ->get();
+
+            if ($uitslagen->isEmpty()) {
+                return back()->with('message', 'Er is geen uitslag beschikbaar voor deze geselecteerde datum');
+            }
+
+            return view('uitslagen.index', compact('uitslagen', 'selectedDate'));
+        }
+
+        return view('uitslagen.index', ['uitslagen' => collect(), 'selectedDate' => null]);
     }
 }
